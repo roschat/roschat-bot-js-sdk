33203d1374adaba157261f872759c65c0e7ddcc0
Vadim Yuldashbaev <narsacgm@gmail.com>
2019-08-14 15:26:07 +0300

    feat: вынес конфигурацию в переменные окружение

--- example/config/index.js
+++ example/config/index.js
@@ -1,6 +1,5 @@
 module.exports = {
-  baseUrl: process.env.NODE_ENV === 'production' ? '' : 'https://ormp.ros.chat',
-  mode: process.env.NODE_ENV,
-  token: '8ed3e59c2c6df2deb4e98b81de5eadbc40cb151b8bb39aab7edd3084bfefb654',
-  name: 'NEW_BRAND_BOT'
+  baseUrl: process.env.BASE_URL || 'https://example.ros.chat',
+  token: process.env.TOKEN || 'your_token_here',
+  name: process.env.NAME || 'your_bot_name'
 }
--- test/config/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  baseUrl: process.env.NODE_ENV === 'production' ? '' : 'https://ormp.ros.chat',
-  mode: process.env.NODE_ENV,
-  token: '8ed3e59c2c6df2deb4e98b81de5eadbc40cb151b8bb39aab7edd3084bfefb654',
-  name: 'NEW_BRAND_BOT'
-}
